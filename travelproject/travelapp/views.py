b208a1ff45ba18ab2191f64f6dad8212534221ac
prabanchcv <prabanchcv@gmail.com>
2022-08-19 15:41:50 +0530

    added

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,13 @@
+from  django.http import HttpResponse
+from django.shortcuts import render
+from .models import Place
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    return render(request,"index.html",{'result':obj})
+
+def about(request):
+    return render(request,"about.html")
+
+def contact(request):
+   return HttpResponse("hello i am contact")
